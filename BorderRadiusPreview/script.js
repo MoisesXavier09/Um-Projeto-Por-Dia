7daeff12529394ce5aaae7e61cb2cf756cd52f4d
MoisesXavier09 <secondmox91@gmail.com>
2024-02-23 03:59:14 -0300

    feat: pré funcionamento criado

--- BorderRadiusPreview/script.js
+++ BorderRadiusPreview/script.js
@@ -1,18 +1,24 @@
-const quadrado = document.querySelector('.quadrado')
+const quadrado = document.querySelector('.square')
 const radius = document.querySelectorAll('input')
 
 
 radius.forEach((input) => {
-    input.addEventListener('input', ({target}) => {onChangeInput})
+    input.addEventListener('input', ({target}) => {onChangeInput(target)})
 })
 
 function onChangeInput(target) {
-    return console.log(target);
-    // switch (target.getAttribute('id')) {
-    //     case "topE":
-    //         quadrado.style.borderTopLeftRadius = target.value + 'px'
-    //         break
-    //     default:
-    //         console.log('banana');
-    // }
+    switch (target.getAttribute('id')) {
+        case "topL":
+            quadrado.style.borderTopLeftRadius = target.value + 'px'
+            break
+        case "topR":
+            quadrado.style.borderTopRightRadius = target.value + 'px'
+            break
+        case "botL":
+            quadrado.style.borderBottomLeftRadius = target.value + 'px'
+            break
+        case "botR":
+            quadrado.style.borderBottomLeftRadius = target.value + 'px'
+            break
+    }
 }
